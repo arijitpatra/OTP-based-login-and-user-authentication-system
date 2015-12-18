24a12dcb903286772a6c8df80fe7342e7715cb79
arijitpatra <ari.clicks@gmail.com>
2015-12-19 03:54:35 +0530

    change
    change

--- success.php
+++ success.php
@@ -4,9 +4,15 @@ session_start();
 <html>
     <head>
         <title>Success</title>
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
     </head>
     <body>
-        <p>SUCCESS Message: </p>
-        <?php echo $_SESSION["mobile"]." ".$_SESSION["name"]." ".$_SESSION["pass"]; ?>
+        <p>
+            <span><i class="fa fa-smile-o" style="font-size: 100px"></i></span>
+        </p>
+        <p>
+            <span>successfully logged in</span>
+        </p>
+        <?php //echo $_SESSION["mobile"]." ".$_SESSION["name"]." ".$_SESSION["pass"]; ?>
     </body>
 </html>
